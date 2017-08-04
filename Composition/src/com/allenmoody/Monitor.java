ab8927b56608928c4637420886f7056008c1d8bf
Allen Moody <allen3kgt@gmail.com>
2017-08-03 23:12:40 -0400

    started Composition

---
+++ Composition/src/com/allenmoody/Monitor.java
@@ -0,0 +1,14 @@
+package com.allenmoody;
+
+public class Monitor {
+    private String model;
+    private String manufacturer;
+    private int size;
+    private Resolution nativeResolution;
+
+    public Monitor(String model, String Manufacturer, int size)
+
+
+
+
+}
